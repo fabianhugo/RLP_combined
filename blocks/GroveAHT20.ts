a99c0c82efa0cd466f29bbc40384a3cce4e4ae34
Hugo <huwg@mailbox.org>
2025-07-16 10:53:49 +0200

    update

--- blocks/GroveAHT20.ts
+++ blocks/GroveAHT20.ts
@@ -3,72 +3,54 @@
  */
 //% groups=['AHT20']
 namespace grove
-{
-    function Read(aht20: grove.sensors.AHT20): { Humidity: number, Temperature: number }
-    {
-        if (!aht20.GetState().Calibrated)
-        {
-            aht20.Initialization();
-            if (!aht20.GetState().Calibrated) return null;
-        }
-
-        aht20.TriggerMeasurement();
-        for (let i = 0; ; ++i)
-        {
-            if (!aht20.GetState().Busy) break;
-            if (i >= 500) return null;
-            basic.pause(10);
-        }
-
-        return aht20.Read();
-    }
-
-    /**
+{    /**
      * Read the temperature(°C) from Grove-AHT20(SKU#101990644)
      */
+    //% subcategory="Sensoren"
     //% group="AHT20"
+    //% block.loc.de="Temperature in °C"
     //% block="[Grove - Temp&Humi Sensor]|Read the temperature(°C))"
     //% weight=3
-    export function aht20ReadTemperatureC(): number
-    {
+    export function aht20ReadTemperatureC(): number {
         const aht20 = new grove.sensors.AHT20();
         const val = Read(aht20);
         if (val == null) return null;
 
-       return Math.round(val.Temperature * 1000) / 1000;   // vorher val.Temperature;
-       
+        return Math.round(val.Temperature * 1000) / 1000;   // vorher val.Temperature;
+
     }
-
-    /**
-     * Read the temperature(°F) from Grove-AHT20(SKU#101990644)
-     */
-    //% group="AHT20"
-    //% block="[Grove - Temp&Humi Sensor]|Read the temperature(°F))"
-    //% weight=2
-    export function aht20ReadTemperatureF(): number
-    {
-        const aht20 = new grove.sensors.AHT20();
-        const val = Read(aht20);
-        if (val == null) return null;
-
-       return Math.round((val.Temperature * 9 / 5 + 32) * 100) / 100;    //vorher val.Temperature * 9 / 5 + 32;
-       
-    }
-
     /**
      * Read the humidity from Grove-AHT20(SKU#101990644)
      */
+    //% subcategory="Sensoren"
     //% group="AHT20"
+    //% block.loc.de="Feuchtigkeit in Prozent"
     //% block="[Grove - Temp&Humi Sensor]|Read the humidity"
     //% weight=1
-    export function aht20ReadHumidity(): number
-    {
+    export function aht20ReadHumidity(): number {
         const aht20 = new grove.sensors.AHT20();
         const val = Read(aht20);
         if (val == null) return null;
 
-       return Math.round(val.Humidity * 1000) / 1000; //vorher return val.humidity
-       
+        return Math.round(val.Humidity * 1000) / 1000; //vorher return val.humidity
+
+    }
+    
+    
+    function Read(aht20: grove.sensors.AHT20): { Humidity: number, Temperature: number } {
+    if (!aht20.GetState().Calibrated) {
+        aht20.Initialization();
+        if (!aht20.GetState().Calibrated) return null;
+    }
+
+    aht20.TriggerMeasurement();
+    for (let i = 0; ; ++i) {
+        if (!aht20.GetState().Busy) break;
+        if (i >= 500) return null;
+        basic.pause(10);
+    }
+
+    return aht20.Read();
     }
 
 }
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-
+enum DistanceUnit {
+    //% block="cm"
+    cm = 1,
+    //% block="inch"
+    inch = 2,
+};
 /**
  * Functions to operate WiFi module.
  */
@@ -199,55 +204,6 @@ namespace IoT {
         return range;
     }
     
-    /**
-     * Read the temperature(°C) from Grove-AHT20(SKU#101990644)
-     */
-    //% subcategory="Sensoren"
-    //% group="AHT20"
-    //% block.loc.de="Temperature in °C"
-    //% block="[Grove - Temp&Humi Sensor]|Read the temperature(°C))"
-    //% weight=3
-    export function aht20ReadTemperatureC(): number {
-        const aht20 = new grove.sensors.AHT20();
-        const val = Read(aht20);
-        if (val == null) return null;
-
-        return Math.round(val.Temperature * 1000) / 1000;   // vorher val.Temperature;
-
-    }
-    /**
-     * Read the humidity from Grove-AHT20(SKU#101990644)
-     */
-    //% subcategory="Sensoren"
-    //% group="AHT20"
-    //% block.loc.de="Feuchtigkeit in Prozent"
-    //% block="[Grove - Temp&Humi Sensor]|Read the humidity"
-    //% weight=1
-    export function aht20ReadHumidity(): number {
-        const aht20 = new grove.sensors.AHT20();
-        const val = Read(aht20);
-        if (val == null) return null;
-
-        return Math.round(val.Humidity * 1000) / 1000; //vorher return val.humidity
-
-    }
-    
-    
-    function Read(aht20: grove.sensors.AHT20): { Humidity: number, Temperature: number } {
-    if (!aht20.GetState().Calibrated) {
-        aht20.Initialization();
-        if (!aht20.GetState().Calibrated) return null;
-    }
-
-    aht20.TriggerMeasurement();
-    for (let i = 0; ; ++i) {
-        if (!aht20.GetState().Busy) break;
-        if (i >= 500) return null;
-        basic.pause(10);
-    }
-
-    return aht20.Read();
-}
 
 
     function waitAtResponse(target1: string, target2: string, target3: string, timeout: number) {
